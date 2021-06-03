988bf039ce777cc64ed3ab2711833bf08b1cf6e5
Julio Cesar <juliocesardemoraes2015@gmail.com>
2021-06-02 23:48:08 -0300

    configurando cors para acesso a api

--- src/server.js
+++ src/server.js
@@ -1,20 +1,44 @@
 const express = require('express');
-//const path = require('path');
+const cors = require('cors');
+
 const routes = require('./routes/routes');
 const db = require('./database/db');
+
 const app = express();
 
 db.connect();
+//habilitando api publica
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       let allowed = true;
+//       if (!origin) allowed = true;
+//       if (!allowedOrigins.includes(origin)) allowed = false;
+//       callback(null, allowed);
+//     },
+//   }),
+// );
+const allowedOrigins = ['http://127.0.0.1:5500'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      let allowed = true;
+
+      // permite conexões sem origin. Exemplo: Mobile Apps
+      if (!origin) allowed = true;
+
+      // verifica se o origin da request consta na lista de autorizados no array allowedOrigins
+      if (!allowedOrigins.includes(origin)) allowed = false;
+
+      callback(null, allowed);
+    },
+  }),
+);
 
 //habilita server para receber dados no formato json
 app.use(express.json());
 
-//rotas
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Início',
-  });
-});
 app.use('/api', routes);
 
 const port = process.env.PORT || 8080;
